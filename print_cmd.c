b3e48e9a9a182530782df6b66d0490bdd1b6780d
Luca Fischer <lucafischer@fastmail.com>
2023-01-15 19:09:07 +0100

    Small correction

--- print_cmd.c
+++ print_cmd.c
@@ -6,7 +6,7 @@
 /*   By: lde-ross < lde-ross@student.42berlin.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 15:39:42 by lde-ross          #+#    #+#             */
-/*   Updated: 2023/01/15 18:51:54 by lde-ross         ###   ########.fr       */
+/*   Updated: 2023/01/15 19:08:52 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,22 @@ void	print_pairs(char **array)
 
 void	print_next_cmd(char **array)
 {
-		ft_printf("%s\n", array[1]);
-		array[1] = ft_memcpy(array[1], array[0], 3);
-		ft_bzero(array[0], 4);
+	ft_printf("%s\n", array[1]);
+	array[1] = ft_memcpy(array[1], array[0], 3);
+	ft_bzero(array[0], 4);
+}
+
+char	**init_array()
+{
+	char **array;
+	    array = malloc(sizeof(char *) * 2);
+		if (!array)
+			return (NULL);
+    array[0] = calloc(sizeof(char), 4);
+    array[1] = calloc(sizeof(char), 4);
+		if (!array[0] || !array[1])
+			return (NULL);
+	return (array);
 }
 
 int	print_cmd(char *str)
@@ -36,13 +49,7 @@ int	print_cmd(char *str)
     static char    **array;
 
     if (!array)  
-       {
-        array = malloc(sizeof(char *) * 2);
-        array[0] = calloc(sizeof(char), 4);
-        array[1] = calloc(sizeof(char), 4);
-       }
-    if (!array)
-        return (0);
+		array = init_array();
     if (!array[0][0])
         array[0] = ft_memcpy(array[0], str, 3);
     else if (!array[1][0])
@@ -53,7 +60,6 @@ int	print_cmd(char *str)
 	}
  	if (are_pairs(array))
 	{
-		printf("array[0]: %s and aray[1] %s", array[0], array[1]);
 		print_pairs(array);
 		ft_bzero(array[0], 4);
 		ft_bzero(array[1], 4);
